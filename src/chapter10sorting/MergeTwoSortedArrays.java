8f06127720d87669c204e9c3257ca07681233f9f
careermonk <careermonk@users.noreply.github.com>
2014-12-21 06:11:47 +0530

    Update MergeTwoSortedArrays.java

--- src/chapter10sorting/MergeTwoSortedArrays.java
+++ src/chapter10sorting/MergeTwoSortedArrays.java
@@ -2,11 +2,11 @@
  * E-Mail           	: info@careermonk.com 
  * Creation Date    	: 2015-01-10 06:15:46 
  * Last modification	: 2006-05-31 
-               by		: Narasimha Karumanchi 
- * Book Title			: Data Structures And Algorithms Made In Java
+               by	: Narasimha Karumanchi 
+ * Book Title		: Data Structures And Algorithms Made In Java
  * Warranty         	: This software is provided "as is" without any 
- * 							warranty; without even the implied warranty of 
- * 							merchantability or fitness for a particular purpose. 
+ * 				warranty; without even the implied warranty of 
+ * 				merchantability or fitness for a particular purpose. 
  * 
  */
 package chapter10sorting;
