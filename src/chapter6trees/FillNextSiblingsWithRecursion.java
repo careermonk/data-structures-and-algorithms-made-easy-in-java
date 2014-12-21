8cbc15a3945251a37d42280c39962d13b5984841
Narasimha Karumanchi <karumanchi.narasimha@gmail.com>
2014-12-22 01:50:38 +0530

    Tree Updates
    Tree Updates

--- src/chapter6trees/FillNextSiblingsWithRecursion.java
+++ src/chapter6trees/FillNextSiblingsWithRecursion.java
@@ -15,5 +15,16 @@
 package chapter6trees;
 
 public class FillNextSiblingsWithRecursion {
-
+	public static void fillNextSiblings(SiblingBinaryTreeNode root) {
+		if (root == null) 
+			return;
+		if (root.getLeft() != null)
+			root.getLeft().setNextSibling(root.getRight());
+		if (root.getRight() != null)
+			if(root.getNextSibling() != null)
+				root.getRight().setNextSibling(root.getNextSibling().getLeft());
+			else	root.getRight().setNextSibling(null);
+		fillNextSiblings(root.getLeft());
+		fillNextSiblings(root.getRight());
+	}
 }
