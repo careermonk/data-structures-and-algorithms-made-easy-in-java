4504ae09a445f9310a0d68aa3b0372a2618ccede
UmakanthSahu <69105946+UmakanthSahu@users.noreply.github.com>
2020-10-01 13:28:58 +0530

    Update NSquareLogNComplexity.java

--- src/chapter01introduction/NSquareLogNComplexity.java
+++ src/chapter01introduction/NSquareLogNComplexity.java
@@ -25,9 +25,9 @@ public class NSquareLogNComplexity {
 	}
 	public static int nSquareLogN(int n){
 		int count = 0;
-		for (int i = 1; i<n;i = i *2){
-			for (int j = 1; j<n;j++){
-				for (int k = 1; k<n;k++){
+		for (int i = 1; i < n; i = i *2){
+			for (int j = 1; j < n; j++){
+				for (int k = 1; k < n; k++){
 					count ++;
 				}
 			}
@@ -36,7 +36,7 @@ public class NSquareLogNComplexity {
 	}
 	public static int nSquareLogN2(int n){
 		int count = 0;
-		for (int i = 1; i<n;i = i+1 ){
+		for (int i = 1; i < n; i = i + 1){
 			for (int j = 1; j<n;j = j*2){
 				for (int k = 1; k<n;k = k + 1){
 					count ++;
@@ -47,9 +47,9 @@ public class NSquareLogNComplexity {
 	}
 	public static int nSquareLogN3(int n){
 		int count = 0;
-		for (int i = 1; i<n;i++){
-			for (int j = 1; j<n;j++){
-				for (int k = 1; k<n;k =  k *2){
+		for (int i = 1; i < n; i++){
+			for (int j = 1; j < n; j++){
+				for (int k = 1; k < n; k = k *2){
 					count ++;
 				}
 			}
